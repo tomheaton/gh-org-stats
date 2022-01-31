541674e01e5140259e96cff43e270234fb84717d
Tom Heaton <thomas.m.heaton@gmail.com>
2022-01-31 23:49:31 +0000

    work++

--- pages/[organization].tsx
+++ pages/[organization].tsx
@@ -1,6 +1,8 @@
 import {GetServerSideProps, NextPage} from "next";
 import {Organization} from "../types/types";
 import styles from "../styles/Organization.module.css";
+import Image from "next/image";
+import { FaStar } from 'react-icons/fa';
 
 type Props = {
     message: string
@@ -25,11 +27,23 @@ const Organization: NextPage<Props> = (props) => {
     const { message, organization } = props;
 
     if (organization) {
+
+        let repos = organization.public_repos;
+        let total_star_count = 0 ;
+
         return (
             <div className={styles.card}>
                 <h1>
                     {organization?.name || organization.login}'s GitHub Stats
                 </h1>
+                <p>
+                    <span>
+                        <FaStar size={30}/>
+                    </span>
+                    Total Stars Earned: {total_star_count}
+                </p>
+                <Image src={organization.avatar_url} height={64} width={64} alt={`${organization.login}'s Avatar`} />
+                <br />
                 <code>
                     {JSON.stringify(organization, null, 4)}
                 </code>
@@ -44,9 +58,6 @@ const Organization: NextPage<Props> = (props) => {
             </h1>
             <p>
                 {message}
-            </p>
-            <p>
-                {JSON.stringify(props)}
             </p>
         </div>
     );
